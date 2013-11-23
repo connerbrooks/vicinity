bf10c2cb4be6c69717564bf71f65ebed09ecd41d
connerbrooks <connerbrooks@gmail.com>
2013-11-22 19:40:59 -0500

    Initial commit for the future

---
+++ build/source/buildConfig/debug/com/google/android/glass/sample/compass/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.google.android.glass.sample.compass;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
